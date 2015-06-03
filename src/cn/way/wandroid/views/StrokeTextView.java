aed260ed7f8f6719a9e39a9f4b72c1228c40d8ed
wayne <wayhap@gmail.com>
2015-06-03 17:21:46 +0800

    StrokeTextView

--- src/cn/way/wandroid/views/StrokeTextView.java
+++ src/cn/way/wandroid/views/StrokeTextView.java
@@ -13,96 +13,100 @@ import android.widget.TextView;
 
 /**
  * StrokeTextView
+ * 
  * @author Wayne
- * @2015年6月3日
- * TODO: strokeColor strokeColorPressed 这两个属性用一个strokeColor来代替
+ * @2015年6月3日 TODO: strokeColor strokeColorPressed 这两个属性用一个strokeColor来代替
  */
 public class StrokeTextView extends TextView {
-    private int strokeColor= Color.TRANSPARENT;
-    private int strokeColorPressed= Color.TRANSPARENT;
-    private int strokeSize=2;
+	private int strokeColor = Color.TRANSPARENT;
+	private int strokeColorPressed = strokeColor;
+	private int strokeSize = 2;
 
-    public StrokeTextView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        init();
-    }
+	public StrokeTextView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init();
+	}
 
-    public StrokeTextView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.StrokeTextView);
-        strokeColor = a.getColor(R.styleable.StrokeTextView_textStrokeColor, strokeColor);
-        strokeColorPressed = a.getColor(R.styleable.StrokeTextView_textStrokeColorPressed, strokeColorPressed);
-        strokeSize = a.getDimensionPixelSize(R.styleable.StrokeTextView_textStrokeSize, strokeColor);
-        a.recycle();
-        init();
-    }
+	public StrokeTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		TypedArray a = context.obtainStyledAttributes(attrs,
+				R.styleable.StrokeTextView);
+		strokeColor = a.getColor(R.styleable.StrokeTextView_textStrokeColor,
+				strokeColor);
+		strokeColorPressed = a.getColor(
+				R.styleable.StrokeTextView_textStrokeColorPressed, strokeColor);
+		strokeSize = a.getDimensionPixelSize(
+				R.styleable.StrokeTextView_textStrokeSize, strokeColor);
+		a.recycle();
+		init();
+	}
 
-    public StrokeTextView(Context context) {
-        super(context);
-        init();
-    }
-    private void init(){
-    }
+	public StrokeTextView(Context context) {
+		super(context);
+		init();
+	}
 
-    @Override
-    protected void onDraw(Canvas canvas) {
+	private void init() {
+	}
 
-        ColorStateList textColor = getTextColors();
-        TextPaint paint = this.getPaint();
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeJoin(Paint.Join.ROUND);
-        paint.setStrokeMiter(10);
+	@Override
+	protected void onDraw(Canvas canvas) {
 
-//        paint.setColor(isPressed()?strokeColorPressed:strokeColor);
-        setTextColor(isPressed()?strokeColorPressed:strokeColor);
-        paint.setStrokeWidth(strokeSize);
+		ColorStateList textColor = getTextColors();
+		TextPaint paint = this.getPaint();
+		paint.setStyle(Paint.Style.STROKE);
+		paint.setStrokeJoin(Paint.Join.ROUND);
+		paint.setStrokeMiter(10);
 
-        //draw stroke
-        super.onDraw(canvas);
+		setTextColor(isPressed() ? strokeColorPressed : strokeColor);
+		paint.setStrokeWidth(strokeSize);
 
-        paint.setStyle(Paint.Style.FILL);
-//        paint.setColor(textColor.getColorForState(getDrawableState(), 0));
-//        setTextColor(textColor);
-        setTextColor(textColor);
-        //draw text
-        super.onDraw(canvas);
+		// draw stroke
+		super.onDraw(canvas);
 
-    }
+		paint.setStyle(Paint.Style.FILL);
+		setTextColor(textColor);
+		// draw text
+		super.onDraw(canvas);
 
-    @Override
-    public void setTextColor(ColorStateList colors) {
-    	super.setTextColor(colors);
-    	updateTextStrokeColors();
-    }
-    @Override
-    public void setTextColor(int color) {
-    	super.setTextColor(color);
-    	updateTextStrokeColors();
-    }
-    @Override
-    protected void drawableStateChanged() {
-        super.drawableStateChanged();
-        if (mTextStrokeColor != null && mTextStrokeColor.isStateful()
-                ) {
-        	updateTextStrokeColors();
-        }
-    }
+	}
 
-    private ColorStateList mTextStrokeColor;
-    private int mCurTextStrokeColor;
-    private void updateTextStrokeColors() {
-    	if (mTextStrokeColor==null) {
+	@Override
+	public void setTextColor(ColorStateList colors) {
+		super.setTextColor(colors);
+		updateTextStrokeColors();
+	}
+
+	@Override
+	public void setTextColor(int color) {
+		super.setTextColor(color);
+		updateTextStrokeColors();
+	}
+
+	@Override
+	protected void drawableStateChanged() {
+		super.drawableStateChanged();
+		if (mTextStrokeColor != null && mTextStrokeColor.isStateful()) {
+			updateTextStrokeColors();
+		}
+	}
+
+	private ColorStateList mTextStrokeColor;
+	private int mCurTextStrokeColor;
+
+	private void updateTextStrokeColors() {
+		if (mTextStrokeColor == null) {
 			return;
 		}
-        boolean inval = false;
-        int color = mTextStrokeColor.getColorForState(getDrawableState(), 0);
-        if (color != mCurTextStrokeColor) {
-            mCurTextStrokeColor = color;
-            inval = true;
-        }
-        if (inval) {
-            invalidate();
-        }
-    }
+		boolean inval = false;
+		int color = mTextStrokeColor.getColorForState(getDrawableState(), 0);
+		if (color != mCurTextStrokeColor) {
+			mCurTextStrokeColor = color;
+			inval = true;
+		}
+		if (inval) {
+			invalidate();
+		}
+	}
 
 }
